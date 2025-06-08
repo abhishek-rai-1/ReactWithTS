d520ad647db7a2e68437a370f34aacf84153455a
abhishek-rai-1 <abhishekrai1972@gmail.com>
2025-06-08 17:25:41 +0530

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Button } from "./components/Button";
-import { User } from "./components/User";
-
 const App = () => {
   return (
     <div>
-      <User name="Abhi" age={22} isStudent={false}>
-        hello
-      </User>
-      <Button label = "click me" disabled = {false} onclick = {() => console.log('button is clicked')}/>
+      <h1>hello</h1>
     </div>
   );
 };
