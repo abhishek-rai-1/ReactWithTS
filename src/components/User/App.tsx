d185f79211413bd5acb1524a860b1ed7943ccac7
abhishek-rai-1 <abhishekrai1972@gmail.com>
2025-06-08 17:24:53 +0530

    User folder

---
+++ src/components/User/App.tsx
@@ -0,0 +1,11 @@
+import { User } from "./User";
+
+const App = () => {
+  return (
+    <User name="Abhi" age={22} isStudent={false}>
+      Hello wolrd
+    </User>
+  );
+};
+
+export default App;
